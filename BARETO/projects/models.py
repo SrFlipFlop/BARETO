e475747fdd853dc7f20d40b7ab3f5f3799b22d6b
Ferran <ferran.obiols.colomina@gmail.com>
2018-01-26 14:51:01 +0100

    add urls and models

--- BARETO/projects/models.py
+++ BARETO/projects/models.py
@@ -3,4 +3,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Projects(models.Model):
+    project_name = models.CharField(max_length=250)
+    project_status = models.CharField(max_length=250)
+    project_start = models.DateTimeField()
+    project_finished = models.DateField()
+    project_notes = models.TextField()
+
+class Assets(models.Model):
+    asset_name = models.CharField(max_length=250)
+    asset_notes = models.TextField()
+    asset_project = models.ForeignKey(Projects, on_delete=models.CASCADE)
+
+class Vulnerabilities(models.Model):
+    vuln_name = models.CharField(max_length=250)
