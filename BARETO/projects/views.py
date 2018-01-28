f21a66c90229aec8e5968ea9870a30343c351aa8
Ferran <ferran.obiols.colomina@gmail.com>
2018-01-28 20:48:04 +0100

    add views for api

--- BARETO/projects/views.py
+++ BARETO/projects/views.py
@@ -1,7 +1,22 @@
 from __future__ import unicode_literals
-
+from rest_framework import viewsets
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(requests):
-    return HttpResponse("Hello")
+from models import *
+from serializers import *
+
+def index(request):
+    return render(request, 'projects/index.html', {})
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+class AssetViewSet(viewsets.ModelViewSet):
+    queryset = Asset.objects.all()
+    serializer_class = AssetSerializer
+
+class VulnerabilityViewSet(viewsets.ModelViewSet):
+    queryset = Vulnerability.objects.all()
+    serializer_class = VulnerabilitySerializer
