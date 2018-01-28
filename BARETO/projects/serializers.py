a749c73c5bdaa05bbc5f20dc378dde1758883296
Ferran <ferran.obiols.colomina@gmail.com>
2018-01-28 20:47:25 +0100

    change serializers to test the functionallity

--- BARETO/projects/serializers.py
+++ BARETO/projects/serializers.py
@@ -1,23 +1,22 @@
+# -*- coding: utf-8 -*-
 from rest_framework import serializers
 
 from models import Project, Asset, Vulnerability
 
-class ProjectSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(required=True, allow_blank=False, max_length=250)
-    status = serializers.CharField(required=False, allow_blank=True, max_length=250)
-    start = serializers.DateTimeField(required=False, allow_blank=True)
-    finished = serializers.DateTimeField(required=False, allow_blank=True)
-    notes = serializers.TextField()
+class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('id', 'name', 'status', 'start', 'finished', 'notes')
 
-    def create(self, data):
-        return Snippet.objects.create(**data)
+class VulnerabilitySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Vulnerability
+        fields = ('id', 'name', 'risk', 'cvss', 'category', 'status', 'description', 'impact', 'recomendation') 
 
-    def update(self, instance, data):
-        instance.name = data.get('name', instance.name)
-        instance.status = data.get('status', instance.status)
-        instance.start = data.get('start', instance.start)
-        instance.finished = data.get('finished', instance.finished)
-        instance.notes = data.get('notes', instance.notes)
-        instance.save()
-        return instance
+class AssetSerializer(serializers.HyperlinkedModelSerializer):
+    #vulnerabilities = VulnerabilitySerializer(many=True, read_only=True)
+    vulnerabilities = VulnerabilitySerializer(many=True)
+
+    class Meta:
+        model = Asset
+        fields = ('id', 'name', 'notes', 'project', 'vulnerabilities')
