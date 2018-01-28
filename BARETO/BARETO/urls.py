be4d864920a714fca20b3aed16deb146c6fc3579
Ferran <ferran.obiols.colomina@gmail.com>
2018-01-28 20:45:08 +0100

    add api routes and tinymce

--- BARETO/BARETO/urls.py
+++ BARETO/BARETO/urls.py
@@ -1,7 +1,17 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework import routers
+
+from projects.views import ProjectViewSet, AssetViewSet, VulnerabilityViewSet
+
+router = routers.DefaultRouter()
+router.register(r'projects', ProjectViewSet)
+router.register(r'assets', AssetViewSet)
+router.register(r'vulnerabilities', VulnerabilityViewSet)
 
 urlpatterns = [
     url(r'^projects/', include('projects.urls')),
+    url(r'^api/', include(router.urls)),
+    url(r'^tinymce/', include('tinymce.urls')),
     #url(r'^admin/', admin.site.urls),
 ]
