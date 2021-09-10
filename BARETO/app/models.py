7bd59d11eaf9ca6531e41f084f2d61f507c4ad05
Ferran <ferran.obiols.colomina@gmail.com>
2021-09-10 17:14:28 +0200

    Changed choises values and new primary key using uuid

--- BARETO/app/models.py
+++ BARETO/app/models.py
@@ -1,45 +1,51 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from uuid import uuid4
 from django.db import models
+from django.contrib.auth.models import Group
 from tinymce.models import HTMLField
 
 PROJECT_STATUS = (
-    ('On course', 'On course'),
-    ('Paused', 'Paused'),
-    ('Finished', 'Finished'),
+    (1, 'Planning'),
+    (2, 'On course'),
+    (3, 'Paused'),
+    (4, 'Finished'),
 )
 
 VULNERABILITY_STATUS = (
-    ('Draft', 'Draft'),
-    ('Ready', 'Ready'),
-    ('Finished', 'Finished'),
+    (1, 'Draft'),
+    (2, 'Ready'),
+    (3, 'Finished'),
 )
 
 VULNERABILITY_RISK = (
-    ('Critical', 'Critical'),
-    ('High', 'High'),
-    ('Medium', 'Medium'),
-    ('Low', 'Low'),
-    ('Informative', 'Informative')
+    (1, 'Informative'),
+    (2, 'Low'),
+    (3, 'Medium'),
+    (4, 'High'),
+    (5, 'Critical')
 )
 
 class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=250)
-    status = models.CharField(max_length=50, choices=PROJECT_STATUS)
+    status = models.IntegerField(choices=PROJECT_STATUS, default=1)
     start = models.DateTimeField(auto_now_add=True, blank=True)
     finished = models.DateTimeField(auto_now_add=True, blank=True)
+    client = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True, blank=True)
     notes = HTMLField(default='TBC')
 
     def __unicode__(self):
         return self.name
 
 class Vulnerability(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=250)
-    risk = models.CharField(max_length=50, choices=VULNERABILITY_RISK)
+    risk = models.IntegerField(choices=VULNERABILITY_RISK, default=1)
     cvss = models.CharField(max_length=250)
     category = models.CharField(max_length=250)
-    status = models.CharField(max_length=50, choices=VULNERABILITY_STATUS)
+    status = models.IntegerField(choices=VULNERABILITY_STATUS, default=1)
     description = HTMLField(default='TBC')
     impact = HTMLField(default='TBC')
     recomendation = HTMLField(default='TBC')
@@ -48,11 +54,12 @@ class Vulnerability(models.Model):
         return self.name
 
 class Template(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=250)
-    risk = models.CharField(max_length=50, choices=VULNERABILITY_RISK)
+    risk = models.IntegerField(choices=VULNERABILITY_RISK)
     cvss = models.CharField(max_length=250)
     category = models.CharField(max_length=250)
-    status = models.CharField(max_length=50, choices=VULNERABILITY_STATUS)
+    status = models.IntegerField(choices=VULNERABILITY_STATUS)
     description = HTMLField(default='TBC')
     impact = HTMLField(default='TBC')
     recomendation = HTMLField(default='TBC')
@@ -61,6 +68,7 @@ class Template(models.Model):
         return self.name
 
 class Asset(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=250)
     notes = HTMLField(default='TBC')
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
